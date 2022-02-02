e1fe0cfaa412735ea25753fe01e180842108c285
JahanaveePotturu <84758162+JahanaveePotturu@users.noreply.github.com>
2022-02-01 21:23:56 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -3,12 +3,12 @@
 # Terraform Providers
 ##############################################################################
 terraform {
-  required_providers {
-    ibm = {
-      source = "IBM-Cloud/ibm"
-      version = ">= 1.36.0"
-    }
-  }
+#   required_providers {
+#     ibm = {
+#       source = "IBM-Cloud/ibm"
+#       version = ">= 1.36.0"
+#     }
+#   }
 }
 
 provider "ibm" {
