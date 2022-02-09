d68a91442ce8658dcb12dd225ae396696d5dd751
JP <Jahanavee.Potturu1@ibm.com>
2022-02-09 11:32:51 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -45,5 +45,5 @@ provider "kubernetes" {
    client_certificate     = data.ibm_container_cluster_config.cluster.admin_certificate
    client_key             = data.ibm_container_cluster_config.cluster.admin_key
    cluster_ca_certificate = data.ibm_container_cluster_config.cluster.ca_certificate
-   load_config_file       = "false"
+   #load_config_file       = "false"
 }
