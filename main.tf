b5f0e153c685198c5273f55d46104f1394ec3ea8
JP <Jahanavee.Potturu1@ibm.com>
2022-03-02 20:14:37 -0500

    update to native resource

--- main.tf
+++ main.tf
@@ -2,49 +2,84 @@
 
 // Create a dedicated worker pool for logging (and eventually monitoring)
 
-#####################################################
-# vpc cluster worker-pool configure
-# Copyright 2022 IBM
-####################################################
-resource "ibm_container_vpc_worker_pool" "logging_pool" {
+
+resource "ibm_container_vpc_worker_pool" "pool" {
   cluster           = var.cluster_name
-  worker_pool_name  = "${var.prefix}-logging"
-  flavor            = var.log_flavor
-  vpc_id            = var.vpc_id
-  worker_count      = var.worker_count
+  worker_pool_name  = var.worker_pool_name
+  flavor            = var.flavor
+  vpc_id            = var.virtual_private_cloud
+  worker_count      = var.worker_nodes_per_zone
   resource_group_id = var.resource_group_id
   labels            = (var.labels != null ? var.labels : null)
   entitlement       = (var.entitlement != null ? var.entitlement : null)
 
-  dynamic "zones" {
-    for_each = var.vpc_zone_names
+  dynamic zones {
+    for_each = (var.worker_zones != null ? var.worker_zones : {})
     content {
-      name      = zones.value
-      subnet_id = zones.vpc_subnet_ids[zones.key]
+      name      = zones.key
+      subnet_id = zones.value.subnet_id
     }
+  }
+
+  dynamic taints {
+    for_each = (var.taints != null ? var.taints : [])
+    content {
+      key    = taints.value.key
+      value  = taints.value.value
+      effect = taints.value.effect
+    }
+  }
+
+  timeouts {
+    create = (var.create_timeout != null ? var.create_timeout : null)
+    delete = (var.delete_timeout != null ? var.delete_timeout : null)
   }
 }
 
+#####################################################
+# vpc cluster worker-pool configure
+# Copyright 2022 IBM
+####################################################
+# resource "ibm_container_vpc_worker_pool" "logging_pool" {
+#   cluster           = var.cluster_name
+#   worker_pool_name  = "${var.prefix}-logging"
+#   flavor            = var.log_flavor
+#   vpc_id            = var.vpc_id
+#   worker_count      = var.worker_count
+#   resource_group_id = var.resource_group_id
+#   labels            = (var.labels != null ? var.labels : null)
+#   entitlement       = (var.entitlement != null ? var.entitlement : null)
 
-resource "null_resource" "taint_logging_pool" {
-    depends_on = [ibm_container_vpc_worker_pool.logging_pool]
+#   dynamic "zones" {
+#     for_each = var.vpc_zone_names
+#     content {
+#       name      = zones.value
+#       subnet_id = zones.vpc_subnet_ids[zones.key]
+#     }
+#   }
+# }
 
-    provisioner "local-exec" {
-        interpreter = ["bash", "-c"]
 
-        command = <<COMMAND
-            echo ${ibm_container_vpc_worker_pool.logging_pool.worker_pool_name} ; \
-            echo ${ibm_container_vpc_worker_pool.logging_pool.cluster} ; \
-            echo ${var.resource_group} ; \
-            ibmcloud login --apikey ${var.ibmcloud_api_key} -r ${var.region} -g ${var.resource_group} --quiet ; \
-            ibmcloud oc worker-pool taint set \
-                --worker-pool ${ibm_container_vpc_worker_pool.logging_pool.worker_pool_name} \
-                --cluster ${ibm_container_vpc_worker_pool.logging_pool.cluster} \
-                --taint logging-monitoring=node:NoExecute -f -q
-        COMMAND
 
-    }
-}
+# resource "null_resource" "taint_logging_pool" {
+#     depends_on = [ibm_container_vpc_worker_pool.logging_pool]
+
+#     provisioner "local-exec" {
+#         interpreter = ["bash", "-c"]
+
+#         command = <<COMMAND
+#             echo ${ibm_container_vpc_worker_pool.logging_pool.worker_pool_name} ; \
+#             echo ${ibm_container_vpc_worker_pool.logging_pool.cluster} ; \
+#             echo ${var.resource_group} ; \
+#             ibmcloud login --apikey ${var.ibmcloud_api_key} -r ${var.region} -g ${var.resource_group} --quiet ; \
+#             ibmcloud oc worker-pool taint set \
+#                 --worker-pool ${ibm_container_vpc_worker_pool.logging_pool.worker_pool_name} \
+#                 --cluster ${ibm_container_vpc_worker_pool.logging_pool.cluster} \
+#                 --taint logging-monitoring=node:NoExecute -f -q
+#         COMMAND
+
+#     }
+# }
 
 data "ibm_container_cluster_config" "cluster" {
   resource_group_id = var.resource_group_id
@@ -73,7 +108,7 @@ data "ibm_container_cluster_config" "cluster" {
 
 
 resource "null_resource" "elasticsearch-namespace" {
-  depends_on = [null_resource.taint_logging_pool]
+  depends_on = [ibm_container_vpc_worker_pool.pool]
 
   provisioner "local-exec" {
     command = <<COMMAND
